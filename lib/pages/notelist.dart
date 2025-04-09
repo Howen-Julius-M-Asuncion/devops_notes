ca21fc66c8a0dee5778884beb4eaac8b96003f46
Renz Gabriel E. Salas <124060701+RenzGabrielSalas@users.noreply.github.com>
2025-04-09 19:53:16 +0800

    Add note pinning

--- lib/pages/notelist.dart
+++ lib/pages/notelist.dart
@@ -22,25 +22,18 @@ class _NotelistPageState extends State<NotelistPage> {
 
     // box.put(
     //   'Notes', {
-    //     "Task" : "Buy some food",
+    //     "title" : "Buy on SM",
+    //     "content" : "I want this one though",
     //     "status" : false,
     //     "pinned" : false,
-    //     "new" : false,
     //   },
     // );
-    //
+    // //
     // noteList.add({
-    //   "Task" : "Buy this one",
+    //   "title" : "Buy this one",
+    //   "content" : "No",
     //   "status" : false,
     //   "pinned" : false,
-    //   "new" : true
-    // });
-    //
-    // noteList.add({
-    //   "Task" : "Buy this two",
-    //   "status" : false,
-    //   "pinned" : false,
-    //   "new" : true
     // });
 
     // box.delete('Notes');
@@ -57,20 +50,21 @@ class _NotelistPageState extends State<NotelistPage> {
       backgroundColor: CupertinoColors.systemGroupedBackground,
       navigationBar: CupertinoNavigationBar(
         automaticallyImplyLeading: false,
-        middle: Text('Tasks'), // TO BE REMOVED FOR MORE IOS-ESQUE LOOK
+        // middle: Text('Notes', style: TextStyle(color: CupertinoTheme.of(context).primaryColor),), // TO BE REMOVED FOR MORE IOS-ESQUE LOOK
         trailing: null, // TO BE ADDED FOR MEMBERS INFO BUTTON
       ),
       child: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+
             // HEADER
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Padding(
-                  padding: EdgeInsets.only(left: 25, top: 25),
+                  padding: EdgeInsets.only(left: 25, top: 15),
                   child: Text(
                     'Notes',
                     style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
@@ -80,31 +74,13 @@ class _NotelistPageState extends State<NotelistPage> {
             ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 25, vertical: 8),
-              child: CupertinoSearchTextField(
-                // borderRadius: BorderRadius.all(Radius.circular(8)),
-              ),
+              child: CupertinoSearchTextField(),
             ),
 
             // NOTE LIST
-            // Expanded(
-            //   child: ListView.builder(
-            //     // itemCount: noteBox.length,
-            //     itemCount: box.length,
-            //     itemBuilder: (context, int index){
-            //       final item = noteList;
-            //       return CupertinoListSection.insetGrouped(
-            //         header: Text('All'),
-            //         children: [
-            //           CupertinoListTile(title: Text(item[index]['Task'])),
-            //         ],
-            //       );
-            //     }
-            //   ),
-            // ),
-
             Expanded(
               child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8,),
+                padding: EdgeInsets.symmetric(horizontal: 8,vertical: 12),
                 child:
                 noteList.isEmpty ?
                 Row(
@@ -118,10 +94,71 @@ class _NotelistPageState extends State<NotelistPage> {
                   itemCount: noteList.length,
                   itemBuilder: (context, int index){
                     final item = noteList;
-                    return CupertinoListTile(
-                      title: Text(item[index]['Task']),
-                      subtitle: Divider(thickness: 1,),
-                      trailing: CupertinoListTileChevron(),
+                    return GestureDetector(
+                      onTap: (){
+                        print('Tapped Item $index, Titled: \"${item[index]['title']}\"');
+                        Navigator.push(context, CupertinoPageRoute(builder: (context) => NotePage()));
+                      },
+                      onLongPress: () {
+                        print('LongPressed Item $index, Titled: \"${item[index]['title']}\"');
+                        showCupertinoDialog(
+                          context: context, 
+                          builder: (context) => CupertinoAlertDialog(
+                            title: Text('Delete this note?'),
+                            content: Text('\"${item[index]['title']}\"'),
+                            actions: [
+                              CupertinoDialogAction(
+                                isDefaultAction: true,
+                                onPressed: (){
+                                  Navigator.pop(context);
+                                },
+                                child: Text('No', style: TextStyle(color: CupertinoColors.systemBlue, fontWeight: FontWeight.bold),)
+                              ),
+                              CupertinoDialogAction(
+                                onPressed: (){
+                                  Navigator.pop(context);
+                                  print('Deleted Item $index, Titled: \"${item[index]['title']}\"');
+                                },
+                                child: Text('Yes', style: TextStyle(color: CupertinoColors.destructiveRed,),)
+                              ),
+                            ],  
+                          )
+                        );
+                      },
+
+                      // Note ITEM
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 18,),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: CupertinoColors.secondarySystemGroupedBackground,
+                            borderRadius: BorderRadius.all(Radius.circular(10))
+                          ),
+                          child: Column(
+                            children: [
+                              CupertinoListTile.notched(
+                                leading: CupertinoButton(
+                                  padding: EdgeInsets.zero,
+                                  child: item[index]['pinned'] == true ? Icon(CupertinoIcons.pin_fill) : Icon(CupertinoIcons.pin),
+                                  onPressed: (){
+                                    // PIN LOGIC
+                                    // DISABLED, PIN ONLY ON PAGE
+
+                                    // setState(() {
+                                    //   item[index]['pinned'] = !item[index]['pinned'];
+                                    // });
+                                    // print('Note \"${item[index]['title']}\", Pinnned? ${item[index]['pinned']}');
+                                  },
+                                ),
+                                title: Text(noteList.isNotEmpty ? item[index]['title'] : ''),
+                                subtitle: Text(noteList.isNotEmpty ? item[index]['content'] : ''),
+                              ),
+                              noteList.isNotEmpty ? Divider(indent: 25,endIndent: 25,height: 2,) : Text('')
+                            ],
+                          ),
+                        ),
+                      ),
+
                     );
                   }
                 ),
@@ -136,11 +173,38 @@ class _NotelistPageState extends State<NotelistPage> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Icon(null),
-                  Text(noteList.isNotEmpty ? '0 Notes' : '${noteList.length} Notes'),
+                  Text(noteList.isEmpty ? '0 Notes' : '${noteList.length} Notes'),
                   CupertinoButton(
                     padding: EdgeInsets.zero,
                     child: Icon(CupertinoIcons.square_pencil),
                     onPressed: (){
+                      Navigator.push(context, CupertinoPageRoute(builder: (context) => NotePage()));
+
+                      // showCupertinoDialog(
+                      //   context: context,
+                      //   builder: (context) => CupertinoAlertDialog(
+                      //     title: Text('Create New Note'),
+                      //     content: Text('Are you sure?'),
+                      //     actions: [
+                      //       CupertinoDialogAction(
+                      //         isDefaultAction: true,
+                      //         onPressed: (){
+                      //           Navigator.pop(context);
+                      //         },
+                      //         child: Text('No', style: TextStyle(color: CupertinoColors.systemBlue, fontWeight: FontWeight.bold),)
+                      //       ),
+                      //       CupertinoDialogAction(
+                      //         onPressed: (){
+                      //           // Navigator.pop(context);
+                      //           Navigator.push(context, CupertinoPageRoute(builder: (context) => NotePage()));
+                      //         },
+                      //         child: Text('Yes', style: TextStyle(color: CupertinoColors.destructiveRed,),)
+                      //       ),
+                      //     ],
+                      //   )
+                      //
+                      // );
+
 
                     }
                   )
